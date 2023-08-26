9b963921ad2c415647d1dd5818fe687f9fc91d9a
djhi12 <ido20002@byui.edu>
2023-08-26 09:05:48 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // import './App.css';
 // import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+// import Expenses from './components/Expenses';
 
 function App() {
+
+  const expenses = [
+    { title: 'Car Insurance', amount: 295.45, date: new Date(8, 26, 2023) },
+    { title: 'Medical', amount: 495.45, date: new Date(8, 26, 2023) },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Expenses expenses={Expenses} />
-      </header>
+    <div className='App'>
+      <div>{expenses[0].date.toISOString}</div>
+      <div>{expenses[0].title}</div>
+      <div>{expenses[0].amount}</div>
     </div>
 
   );
