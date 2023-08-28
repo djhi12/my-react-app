f312d576e46bc05a40701f87d1c1b8c1c8d8fc01
djhi12 <ido20002@byui.edu>
2023-08-28 15:54:45 +0800

    Initial commit

---
+++ src/components/expense-details/ExpenseFilter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../expense-details/ExpenseFilter.css';
+
+function ExpensesFilter() {
+    const dropdownChangeHadler = (event) => {
+        console.log(event.target.value);
+    };
+
+    return (
+        <div>
+            <label>Filter by year</label>
+            <select className="form-select" onChange={dropdownChangeHadler} aria-label="Default select example">
+                <option selected>Filter year</option>
+                <option value="2022">One</option>
+                <option value="2021">Two</option>
+                <option value="2020">Three</option>
+                <option value="2019">Three</option>
+            </select>
+        </div>
+    );
+}
+
+export default ExpensesFilter;
